ea32473be21aa634f5fb17921a1f54bdecf5c37b
wangbin <a395zxb@qq.com>
2019-07-08 17:13:40 +0800

    更新pom

--- sp-yanwh-provider/src/main/java/com/shop/SpYanwhProviderApplication.java
+++ sp-yanwh-provider/src/main/java/com/shop/SpYanwhProviderApplication.java
@@ -1,9 +1,13 @@
 package com.shop;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@MapperScan("com.shop.dao")
 public class SpYanwhProviderApplication {
 
     public static void main(String[] args) {
